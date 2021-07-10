04cb7a4909a3be2818422c459ede5aeda1d30d4a
Sohail Haqyar <haqyarsohail@gmail.com>
2021-07-11 02:44:27 +0300

    new google id

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,3 @@
 export const JWT_SECRET = 'XwPp9xazJ0ku5CZnlmgAx2Dld8SHkAeT';
 export const GOOGLE_CLIENT_ID =
-  '63096940458-pttpa4kr77dhpsj1vffgtrs580vd7gq1.apps.googleusercontent.com';
+  '63096940458-fvomofr8jbr1uiphkcdv0gcqpgib5421.apps.googleusercontent.com';
